87f778f315768535952f2146ecea9274aa3f37eb
joao victor cordeiro <joaocordeiro2134@gmail.com>
2023-03-21 22:27:16 -0300

    add trabalho 2

---
+++ trabalho2/main.js
@@ -0,0 +1,24 @@
+const createEl = () => {
+    // Criar elemento
+    var titulo = document.createElement("h1");
+
+    // Cria um texto para ser inserido dentro da tag h1
+    var texto = document.createTextNode("Um título qualquer");
+    
+    // Adiciona o texto dentro da tag H1
+    titulo.appendChild(texto);
+
+    // Insere o elemento filho (titulo) ao elemento pai (body)
+    document.body.insertBefore(titulo, document.getElementById("content")); 
+}
+
+const deleteEl = () => {
+    document.body.removeChild(document.getElementsByTagName('h1'));
+}
+
+const replaceEl = () => {
+    const newNode = document.createTextNode("Um título qualquer alterado");
+    const element = document.getElementsByTagName("h1");
+        
+    element.replaceChild(newNode, element);
+}
